8abe671ebb64e1a5d2fce68ff2d349899f3c1d41
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-09-24 14:55:45 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect} from "react";
 
 function App() {
-    //const [a, b] = useState(c)
+    const [dogPic, setDogPic] = useState([])
     const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => response.json())
-        .then((data) => data.img);
+        .then((data) => {
+            setDogPic(data.message);
         setIsLoaded(true);
         })
 
@@ -16,7 +17,7 @@ function App() {
     if (!isLoaded) return <p>Loading...</p>;
 
     return (
-        <img alt="A Random Dog">{data.img} </img>
+        <img src={dogPic} alt="A Random Dog"></img>
 
     )
 }
